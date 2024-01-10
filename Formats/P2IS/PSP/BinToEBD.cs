4de9cf8d751ba12d61effec38b0fe3127cf39dfa
Megaflan <smtivesp@gmail.com>
2024-01-10 12:31:19 +0100

    Better control code parsing (incomplete)

--- Formats/P2IS/PSP/BinToEBD.cs
+++ Formats/P2IS/PSP/BinToEBD.cs
@@ -7,28 +7,25 @@ using Yarhl.FileFormat;
 using Yarhl.IO;
 using Yarhl.Media.Text.Encodings;
 using ClassicPersonaToolkit.Helpers.P2IS.PSP.Models;
+using System.Net.Sockets;
 
 namespace ClassicPersonaToolkit.Formats.P2IS.PSP
 {
     public class BinToEBD : IConverter<BinaryFormat, EBD>
     {
-        Dictionary<ushort, string> controlCodes = new Dictionary<ushort, string>
+        Dictionary<byte, string> controlCodes = new Dictionary<byte, string>
         {
-            { 0x1101, "\n" },
-            { 0x1102, "{1102}" },
-            { 0x1103, "{1103}" },
-            { 0x1104, "{1104}" },
-            { 0x1105, "{1105}" },
-            { 0x1106, "{1106}" },
-            { 0x1107, "{1107}" },
-            { 0x1108, "{1108}" },
-            { 0x1109, "{1109}" },
-            { 0x1110, "{1110}" },
-            { 0x1112, "{FIRST_NAME}" },
-            { 0x1113, "{LAST_NAME}" },
-            { 0x1205, "{1205}" },
-            { 0x1120, " " }
+            { 0x05, "WAIT" },
+            { 0x12, "FNAME" },
+            { 0x13, "LNAME" },
         };
+
+        Dictionary<byte, string> specialChars = new Dictionary<byte, string>
+        {
+            { 0x01, "\n" },
+            { 0x20, " " }
+        };
+
         public EBD Convert(BinaryFormat source)
         {
             if (source == null)
@@ -65,9 +62,30 @@ namespace ClassicPersonaToolkit.Formats.P2IS.PSP
                     while (buffer != 0x1103)
                     {
                         buffer = reader.ReadUInt16();
-                        sb.Append(controlCodes.ContainsKey(buffer) ? controlCodes[buffer] : ((char)buffer).ToString());
+
+                        if (buffer == 0x1431)
+                            break;
+
+                        if ((byte)(buffer >> 12) == 1) //If the first 4 bits are 0001, it's a control code.
+                        {
+                            var argument = (byte)((buffer >> 8) & 0x0F); //Extract the second group, this defines the arguments.
+                            var type = (byte)(buffer & 0xFF); //Extract the third and four group, this defines the control code type.
+
+                            if (specialChars.ContainsKey(type))
+                                sb.Append(specialChars[type]);
+                            else
+                            {
+                                sb.Append("{");
+                                sb.Append(controlCodes.ContainsKey(type) ? controlCodes[type] : "UNK" + type);
+                                sb.Append(":");
+                                sb.Append(argument);
+                                sb.Append("}");
+                            }                            
+                        }
+                        else
+                            sb.Append(((char)buffer).ToString());
                     }
-                    
+
                     ebd.Entries.Add(new EBD.Entry()
                     {
                         Id = id,
